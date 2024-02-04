a52d5d713d24b30d2674806bc1d77ae8df6f015b
SanjayKumar0072 <b.sanjaykumar2000@gmail.com>
2024-02-04 21:47:37 +0530

    config file update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs-extra';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'copy-files',
+      writeBundle() {
+        fs.copySync('sitemap.xml', 'dist/sitemap.xml');
+        fs.copySync('robots.txt', 'dist/robots.txt');
+      },
+    },
+  ],
+});
