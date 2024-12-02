5e2eb4bdf4fe9fecd2cc0d078532995451b295ca
SanjayKumar0072 <b.sanjaykumar2000@gmail.com>
2024-12-02 13:07:58 +0530

    path checks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState , useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/home';
 import Resume from './components/resumePage';
 
